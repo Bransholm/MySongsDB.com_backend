77808f80ca28d6cf23e5be6113471646f2a44cc0
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-09-15 19:13:22 +0200

    album routes - the basics

--- server.js
+++ server.js
@@ -10,8 +10,83 @@ app.use(cors());
 
 //Routes goes here...
 
-
-
 app.listen(port, () => {
   console.log(`The sever is running on port ${port}\nEnjoy your day :)`);
+});
+
+//////// ALBUM ROUTS ////////
+
+// READ all albums
+app.get("/albums", (request, response) => {
+  const query = "SELECT * FROM albums";
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// READ one albums
+app.get("/albums/:albumId", (request, response) => {
+  const albumId = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const query = "SELECT * FROM users WHERE id=?";
+  const values = [albumId];
+
+  connection.query(query, values, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results[albumId]);
+    }
+  });
+});
+
+// CREATE albums
+app.post("/albums", (request, response) => {
+  const album = request.body;
+  console.log(album);
+
+  const values = [album.albumName, album.edition, album.year, album.albumImage];
+  const query =
+    "INSERT INTO sers (albumName, edition, year, albumImage) VALUES (?,?,?,?)";
+
+  connection.query(query, values, (err, results, fields) => {
+    response.json(results);
+  });
+});
+
+// UPDATE albums
+app.put("/album/:albumId", async (request, response) => {
+  const albumId = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const albumBody = request.body;
+
+  const values = [
+    albumBody.albumName,
+    albumBody.edition,
+    albumBody.year,
+    albumBody.albumImage,
+    albumId,
+  ];
+  const query =
+    "UPDATE userse SET albumName=?, edition=?, year=?, albumImage=? VALUES WHERE albumId=?";
+  connection.query(query, values, (err, results, fields) => {
+    response.json(results);
+  });
+});
+
+// DELETE albums
+app.delete("/albums/:albumId", async (request, response) => {
+  const id = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const values = [id];
+  const query = "DELETE FROM album WHERE id=?";
+
+  connection.query(query, values, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results);
+    }
+  });
 });
