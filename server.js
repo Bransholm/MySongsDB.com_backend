9b42bdbdfd19a069da401fcc0a23e916d9bc4060
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-09-20 14:19:38 +0200

    big merge

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import fs from "fs/promises";
 import dbConnection from "./database.js";
@@ -34,7 +34,7 @@ app.get("/artist", (request, response) => {
   });
 });
 
-//////// ------------- ALBUM ROUTS ------------- ////////
+//////// ALBUM ROUTS ////////
 
 // READ all albums
 app.get("/albums", (request, response) => {
@@ -103,6 +103,102 @@ app.delete("/albums/:albumId", async (request, response) => {
   const query = "DELETE FROM album WHERE id=?";
 
   dbConnection.query(query, values, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results);
+    }
+  });
+});
+import mysql from "mysql2";
+import express from "express";
+import cors from "cors";
+import fs from "fs/promises";
+import connection from "./Data/data_connections.js";
+
+// const app = express();
+// const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+if (process.env.MYSQL_CERT) {
+  dbconfig.ssl = { cs: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") };
+}
+
+app.listen(port, () => {
+  console.log(`The sever is running on port ${port}\nEnjoy your day :)`);
+});
+
+//////// ------------- ALBUM ROUTS ------------- ////////
+
+// READ all albums
+app.get("/albums", (request, response) => {
+  const query = "SELECT * FROM albums";
+  connection.query(query, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// READ one albums
+app.get("/albums/:albumId", (request, response) => {
+  const albumId = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const query = "SELECT * FROM users WHERE id=?";
+  const values = [albumId];
+
+  connection.query(query, values, (err, results, fields) => {
+    if (err) {
+      console.log(err);
+    } else {
+      response.json(results[albumId]);
+    }
+  });
+});
+
+// CREATE albums
+app.post("/albums", (request, response) => {
+  const album = request.body;
+  console.log(album);
+
+  const values = [album.albumName, album.edition, album.year, album.albumImage];
+  const query =
+    "INSERT INTO sers (albumName, edition, year, albumImage) VALUES (?,?,?,?)";
+
+  connection.query(query, values, (err, results, fields) => {
+    response.json(results);
+  });
+});
+
+// UPDATE albums
+app.put("/album/:albumId", async (request, response) => {
+  const albumId = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const albumBody = request.body;
+
+  const values = [
+    albumBody.albumName,
+    albumBody.edition,
+    albumBody.year,
+    albumBody.albumImage,
+    albumId,
+  ];
+  const query =
+    "UPDATE userse SET albumName=?, edition=?, year=?, albumImage=? VALUES WHERE albumId=?";
+  connection.query(query, values, (err, results, fields) => {
+    response.json(results);
+  });
+});
+
+// DELETE albums
+app.delete("/albums/:albumId", async (request, response) => {
+  const id = request.params.albumId; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const values = [id];
+  const query = "DELETE FROM album WHERE id=?";
+
+  connection.query(query, values, (err, results, fields) => {
     if (err) {
       console.log(err);
     } else {
