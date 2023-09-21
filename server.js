29ad142a34afc2f6e3a77d13a8e8a59f418069c7
Gorm-git <124032495+Gorm-git@users.noreply.github.com>
2023-09-21 14:57:14 +0200

    Så er der lidt flere routes til tracks
    Jeg mangler stadig at lave update og delete tracks

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import fs from "fs/promises";
 import dbConnection from "./data/database.js";
@@ -34,14 +34,68 @@ app.get("/artists", (request, response) => {
   });
 });
 
+//////// TRACKS ROUTES ////////
+
+// READ all tracks //
+app.get("/tracks", (request, response) => {
+  const query = "SELECT * FROM artists ORDER by tracksName";
+  dbConnection.query(query, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// READ one track //
+
+app.get("/tracks/:trackID", (request, response) => {
+  const id = request.params.trackID;
+  const query = "SELECT * FROM tracks WHERE id=?";
+  const values = [id];
+
+  dbConnection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
+// Create a track //
+
+app.post("/tracks", (request, response) => {
+  const track = request.body;
+  console.log(track);
+
+  const values = [
+    track.trackName,
+    track.length,
+    track.creationYear,
+    track.genre,
+  ];
+  const query =
+    "INSERT INTO tracks (trackName, length, creationYear, genre) VALUES (?,?,?,?)";
+
+  dbConnection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
+});
+
 //////// ALBUM ROUTS ////////
 
 // READ all albums
 app.get("/albums", (request, response) => {
   const query = "SELECT * FROM albums";
-  dbConnection.query(query, (err, results, fields) => {
-    if (err) {
-      console.log(err);
+  dbConnection.query(query, (error, results, fields) => {
+    if (error) {
+      console.log(error);
     } else {
       response.json(results);
     }
