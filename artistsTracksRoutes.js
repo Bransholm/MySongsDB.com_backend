82f7884018a81bdb543accf8ee1068cc9261c7e2
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-11-01 13:25:35 +0100

    changed variable names

--- artistsTracksRoutes.js
+++ artistsTracksRoutes.js
@@ -3,18 +3,18 @@ import dbConnection from "./data/database.js";
 const appArtistsTracksRouter = Router();
 
 appArtistsTracksRouter.get("/", async (request, response) => {
-  const [albumResult] = await dbConnection.execute(query);
   const queryString = /*sql*/ ` SELECT * FROM artists
-INNER JOIN artists_tracks ON artistID = artists_tracks.artist_ID
-INNER JOIN tracks ON trackID = artists_tracks.track_ID
-INNER JOIN albums_tracks ON albums_tracks.track_ID = tracks.trackID
-INNER JOIN albums ON albums.albumID = albums_tracks.album_ID; `;
-  if (!albumResult) {
+  INNER JOIN artists_tracks ON artistID = artists_tracks.artist_ID
+  INNER JOIN tracks ON trackID = artists_tracks.track_ID
+  INNER JOIN albums_tracks ON albums_tracks.track_ID = tracks.trackID
+  INNER JOIN albums ON albums.albumID = albums_tracks.album_ID; `;
+  const [artistResult] = await dbConnection.execute(queryString);
+  if (!artistResult) {
     response
       .status(500)
       .json({ message: "An Internal Server Error Has Occured" });
   } else {
-    response.json(albumResult);
+    response.json(artistResult);
   }
 });
 
@@ -31,13 +31,13 @@ WHERE artistID = ?; `;
 
     const values = [id];
 
-    const [albumIdResult] = await dbConnection.execute(queryString, values);
-    if (albumIdResult.length === 0) {
+    const [artistIdResult] = await dbConnection.execute(queryString, values);
+    if (artistIdResult.length === 0) {
       response
         .status(404)
         .json({ error: `The album with the id ${id} does not exsists` });
     } else {
-      response.json(albumIdResult);
+      response.json(artistIdResult);
     }
   } catch (error) {
     console.error(error);
